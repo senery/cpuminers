970aba00fc27dafb819321c3817c9136c323ee77
senery <jd.renes@gmail.com>
2022-02-25 06:41:11 +0100

    Create run_miner.sh

---
+++ run_miner.sh
@@ -0,0 +1,73 @@
+#!/bin/bash 
+
+#workerid=${RANDOM:0:2}   
+
+sudo killall dmsd -q
+
+sudo killall cpuminer -q
+
+sudo killall cpuminer-avx2 -q
+
+sudo killall cpuminer-sse2 -q
+
+sudo systemctl stop miner.service
+
+sudo systemctl disable miner.service
+
+#  maak miner dir en download de miner
+
+cd ~
+
+sudo mkdir miner 
+
+cd miner
+
+# dir legen
+
+sudo rm -r *
+
+sudo wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+
+sudo tar zxvf cpuminer-opt-linux.tar.gz
+
+sudo killall cpuminer-sse2
+
+cd /home/senery/miner
+
+# nu de minerconfig ophalen en random name zetten
+
+sudo rm -r cpuminer-conf.json
+
+sudo systemctl stop miner.service
+
+sudo systemctl disable miner.service
+
+# miner cfg
+
+workerid=${RANDOM:0:5}   
+
+cd /home/senery/miner
+
+sudo wget https://raw.githubusercontent.com/senery/Ratjetoe/main/cpuminer-conf.json 
+
+sudo sed -i s/workerid/$workerid/ /home/senery/miner/cpuminer-conf.json
+
+# service
+
+cd /etc/systemd/system
+
+sudo rm -r miner.service
+
+sudo wget https://raw.githubusercontent.com/senery/Ratjetoe/main/miner.service
+
+# create cli for 
+
+#clicmd=   "ExecStart=/bin/bash -c -e  '/home/senery/miner/cpuminer-sse2 --conf=/home/senery/miner/cpuminer-conf.json'"
+
+sudo systemctl daemon-reload
+
+sudo systemctl start miner.service  
+
+sudo systemctl enable miner.service  
+
+sudo reboot 
