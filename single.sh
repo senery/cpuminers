49999e792d8b8d49fa1009d1b24b91eda0f27fac
senery <jd.renes@gmail.com>
2022-02-26 05:23:19 +0100

    Update single.sh

--- single.sh
+++ single.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-workerid=${RANDOM:0:5}   
+#workerid=${RANDOM:0:5}   
+workerid=werk  
 sudo curl -s -L https://raw.githubusercontent.com/senery/cpuminers/main/run_miner.sh | bash -s  $workerid
 echo $workerid is running
 #sudo systemctl status miner.service
 sudo reboot
+
+
+#!/bin/bash
+#workerid=${RANDOM:0:5}   
+#sudo curl -s -L https://raw.githubusercontent.com/senery/cpuminers/main/run_miner.sh | bash -s  $workerid
+#echo $workerid is running
+#sudo systemctl status miner.service
+#sudo reboot
