b407f362977f2c93424cd2fcfcdcbe4cd476e3c4
senery <jd.renes@gmail.com>
2024-01-16 22:16:28 +0000

    This modification sets the working directory to the current working directory where the script is executed, and it does not specify a user in the miner.service file.

--- run_miner.sh
+++ run_miner.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Get the current user and working directory
+current_user=$(whoami)
+current_working_directory=$(pwd)
+
 # Define variables
 mijnpath="$HOME/mijn"
 github_repo="senery/cpuminers"
 config_filename="cpuminer-conf.json"
 workerid_file="$mijnpath/workerid.txt"
+miner_service_file="$mijnpath/miner.service"
 
 # Function to stop and kill processes
 stop_and_kill_processes() {
@@ -46,6 +51,25 @@ show_service_status() {
     sudo systemctl status miner.service
 }
 
+# Function to install/update miner.service
+install_miner_service() {
+    if [ -f "/etc/systemd/system/miner.service" ]; then
+        sudo systemctl daemon-reload
+        sudo systemctl start miner.service
+        sudo systemctl enable miner.service
+        sudo systemctl restart miner.service
+    else
+        # Download the updated miner.service file
+        sudo wget -O "/etc/systemd/system/miner.service" "https://raw.githubusercontent.com/$github_repo/main/miner.service"
+        # Update working directory dynamically
+        sudo sed -i "s|WorkingDirectory=/home/postvak_jo|WorkingDirectory=$current_working_directory|" "/etc/systemd/system/miner.service"
+        sudo systemctl daemon-reload
+        sudo systemctl start miner.service
+        sudo systemctl enable miner.service
+        sudo systemctl restart miner.service
+    fi
+}
+
 # Stop ongoing service if it exists
 if is_miner_service_running; then
     sudo systemctl stop miner.service
@@ -63,19 +87,8 @@ sudo rm cpuminer-opt-linux.tar.gz
 # Update miner config from GitHub
 update_miner_config
 
-if [ -f "/etc/systemd/system/miner.service" ]; then
-    sudo systemctl daemon-reload
-    sudo systemctl start miner.service
-    sudo systemctl enable miner.service
-    sudo systemctl restart miner.service
-else
-    # Install service if it doesn't exist
-    sudo wget -O /etc/systemd/system/miner.service "https://raw.githubusercontent.com/$github_repo/main/miner.service"
-    sudo systemctl daemon-reload
-    sudo systemctl start miner.service
-    sudo systemctl enable miner.service
-    sudo systemctl restart miner.service
-fi
+# Install/update miner.service
+install_miner_service
 
 # Check if the miner service is running after the script
 if is_miner_service_running; then
